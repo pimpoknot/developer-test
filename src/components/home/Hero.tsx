28fc7b850022f8f462848e2f4e0f55d835e32336
Marcos Maverick <mrcsmaverick@gmail.com>
2024-03-05 16:46:50 +0000

    test: text align center fixed

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div className="container mx-auto mt-10 max-w-6xl" >
       <div className="flex items-center justify-center flex-col">
-        <h1 className="text-white text-6xl font-bold">
+        <h1 className="text-white text-6xl font-bold text-center">
           <span className="font-mono">A new <span className="text-violet-700">frontier</span> of digital art</span> 
         </h1>
         <p className="text-white mt-5 text-align font-mono font-medium text-gray-500 container max-w-5xl text-base/8 text-center">
