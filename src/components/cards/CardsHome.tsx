6ddac834036c922f3dd54f3ddf55bf27e2673a21
Marcos Maverick <mrcsmaverick@gmail.com>
2024-03-05 21:51:35 +0000

    feat: adding link to each card page

--- src/components/cards/CardsHome.tsx
+++ src/components/cards/CardsHome.tsx
@@ -1,15 +1,19 @@
 import { arts } from "@/infra/api/data";
 import CardArt from "./CardsArt";
+import Link from "next/link";
 
 
 export default function CardsHome() {
   return (
     <div className="container mx-auto mt-8 px-4">
       <div className="flex items-center justify-between mb-4">
+        {/* ... */}
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 hover:*:bg-gray-500 cursor-pointer">
         {arts.map(art => (
-          <CardArt key={art.id} art={art} />
+          <Link key={art.id} href={`/${art.id}`} passHref>
+            <CardArt art={art} />
+          </Link>
         ))}
       </div>
     </div>
